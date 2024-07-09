dcd6d3608cd42633eef514b5422de718ead1146a
Kaan Kani İnce <kkince@newmind.ai>
2024-07-09 13:43:02 +0300

    Searc-autocomplete-with-api created

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import SearchAutocompleteWithApi from "./components/search-autocomplete-with-api";
+
+function App() {
+  return (
+    <div className="App">
+      {/* Search Autocomplete */}
+      <SearchAutocompleteWithApi/>
+    </div>
+  );
+}
+
+export default App;
